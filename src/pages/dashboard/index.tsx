864908f8d8153450b97c52cf99c64e15e779f456
tolu13 <tolulopeolanrewaju8@fmail.com>
2025-10-10 14:06:55 +0100

    added

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -36,7 +36,7 @@ export const DashBoardLayout = () => {
       {/* Sidebar - slide in/out on mobile, fixed on desktop */}
       <aside
         className={`
-          fixed inset-y-0 left-0 z-40 w-64 bg-[#BF7587] text-white transform
+          fixed inset-y-0 left-0 z-40 w-64  text-white transform
           transition-transform duration-300 ease-in-out
           lg:relative lg:translate-x-0 lg:h-screen  lg:flex lg:flex-col lg:overflow-y-auto
           ${sidebarOpen ? "translate-x-0" : "-translate-x-full"}
