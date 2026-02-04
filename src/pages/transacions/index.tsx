3329e4efe224eeb22bbf42f104a8dbdd2027ce11
tolu13 <tolulopeolanrewaju8@fmail.com>
2026-02-04 09:31:54 +0100

    changed transaction url

--- src/pages/transacions/index.tsx
+++ src/pages/transacions/index.tsx
@@ -25,7 +25,7 @@ export const Transactions = () => {
       if (!token) return;
       setLoading(true);
       try {
-        const res = await axios.get("http://localhost:5000/orders/user", {
+        const res = await axios.get("https://nexatrade-weld.vercel.app/orders/user", {
           headers: { Authorization: `Bearer ${token}` },
         });
         setOrders(res.data);
